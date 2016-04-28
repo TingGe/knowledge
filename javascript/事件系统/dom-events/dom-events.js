a22da2223a5efdb04828495e2783e9e911842efe
TingGe <505253293@163.com>
2016-04-28 18:21:05 +0800

    added javascript.algoritms

--- javascript/事件系统/dom-events/dom-events.js
+++ javascript/事件系统/dom-events/dom-events.js
@@ -7,7 +7,8 @@ var win = window,
 
 var utils = {
   addListener: null,
-  removeListener: null
+  removeListener: null,
+  stop: null
 };
 
 if (typeof win.addEventListener === 'function') {
@@ -32,3 +33,24 @@ if (typeof win.addEventListener === 'function') {
     el['on' + type] = null;
   };
 }
+
+/**
+ * 设计模式：外观模式
+ */
+// 将浏览器的差异隐藏在一个外观方法下面
+utils.stop = function(e) {
+  // 其它浏览器
+  if (typeof e.preventDefault === "function") {
+    e.preventDefault();
+  }
+  if (typeof e.stopPropagation === "function") {
+    e.stopPropagation();
+  }
+  // IE
+  if (typeof e.returnValue === "boolean") {
+    e.returnValue = false;
+  }
+  if (typeof e.cancelBubble === "boolean") {
+    e.cancelBubble = true;
+  }
+};
